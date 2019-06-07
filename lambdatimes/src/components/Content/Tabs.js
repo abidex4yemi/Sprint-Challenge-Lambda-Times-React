71cb4fdb0c99190b3e0418d8285fc9f025c038cf
abidex4yemi <abidex4yemi@gmail.com>
2019-06-07 11:29:09 +0100

    Implement tab selection functionality

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Tab from './Tab';
+import uuid from 'uuid';
 const Tabs = props => {
-  return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-      </div>
-    </div>
-  );
+	const { tabs, changeSelected, selectedTab } = props;
+
+	return (
+		<div className="tabs">
+			<div className="topics">
+				<span className="title">TRENDING TOPICS:</span>
+				{tabs.map(tab => <Tab key={uuid()} tab={tab} changeSelected={changeSelected} selectedTab={selectedTab} />)}
+			</div>
+		</div>
+	);
 };
 
 // Make sure to use PropTypes to validate your types!
